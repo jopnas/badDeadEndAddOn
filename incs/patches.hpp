84c0bc78a88392786a1e5359c4d89fc2695decdd
Jopnas <jonas.grunwald@gmx.de>
2016-07-05 13:04:23 +0200

    splited classes to files

---
+++ incs/patches.hpp
@@ -0,0 +1,13 @@
+class CfgPatches
+{
+    class bde
+    {
+        units[]={}
+        weapons[]={}
+        magazines[]={}
+        ammo[]={}
+        vehicles[]={}
+        requiredVersion=0.1;
+        requiredAddons[]={}
+    }
+}
