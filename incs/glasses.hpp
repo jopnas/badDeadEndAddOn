f551242e874f13929de4238efe9326b3239953e4
jopnas <jonas.grunwald@gmx.de>
2016-09-24 00:14:46 +0200

    own test gasmask model

--- incs/glasses.hpp
+++ incs/glasses.hpp
@@ -1,11 +1,11 @@
 class cfgGlasses
 {
     class None;
-    class bde_respirator:None
+    class bde_gasmask:None
     {
         author = "jopnas";
         displayname = "Gasmask";
-        model = "\A3\characters_f_beta\heads\glasses\g_combat";
+        model = "\bde\models\gasmask";
         picture = "\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
         identityTypes[] = {};
         mass = 4;
