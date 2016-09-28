bec6c38c708770550ec0d371dbb7b77550602d40
jopnas <jonas.grunwald@gmx.de>
2016-09-28 21:20:43 +0200

    working gasmask, working wasted gasmask

--- incs/glasses.hpp
+++ incs/glasses.hpp
@@ -18,7 +18,7 @@ class CfgGlasses
     {
         author = "NotSure";
         displayname = "Gasmask (wasted filter)";
-        model = "\bde\models\bde_gasmask.p3d";
+        model = "\bde\models\bde_gasmask_wasted.p3d";
         picture = "\A3\Characters_F\data\ui\icon_g_combat_CA.paa";
 		identityTypes[] = {"NoGlasses",0,"G_NATO_default",0,"G_NATO_casual",0,"G_NATO_pilot",0,"G_NATO_recon",0,"G_NATO_SF",0,"G_NATO_sniper",0,"G_NATO_diver",0,"G_IRAN_default",0,"G_IRAN_diver",0,"G_GUERIL_default",0,"G_HAF_default",0,"G_CIVIL_female",0,"G_CIVIL_male",0};
 
